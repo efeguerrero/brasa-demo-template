c4b1204d26e77e1695fcb7064f4c97b2ff9c2bce
efeguerrero <juanfg.8640@gmail.com>
2023-03-17 18:43:51 -0300

    NavBar done and scroll to top btn added

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import { React, useRef, useState } from 'react';
+import MenuIcon from './menuIcon/MenuIcon';
+
+//Logo url in public and alt text
+const logo = '/assets/navbar/logo.svg';
+const logoAlt = 'Website Logo';
+
+//Component Imports
+import Link from './link/Link.jsx';
+import Socials from './socials/Socials';
+
+//links Import
+import links from './links';
+
+//Styles Import
+import styles from './navbar.module.css';
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const refDrawer = useRef(null);
+  const menuToggle = () => {
+    refDrawer.current.classList.toggle(`${styles.drawerActive}`);
+    setIsOpen((prev) => !prev);
+    // document.body.classList.toggle('no-scroll');
+  };
+
+  return (
+    <nav className={styles.navContainer}>
+      <div className={styles.navIcons}>
+        <img src={logo} alt={logoAlt} className={styles.navLogo} />
+        <div className={styles.linksContainerDesktop}>
+          {links.map((link) => {
+            return <Link url={link.url} linkName={link.name} />;
+          })}
+        </div>
+        <MenuIcon menuToggle={menuToggle} isOpen={isOpen} />
+      </div>
+      <div ref={refDrawer} className={styles.drawer}>
+        <div className={styles.linksContainerDrawer}>
+          {links.map((link) => {
+            return (
+              <Link
+                url={link.url}
+                linkName={link.name}
+                menuToggle={menuToggle}
+              />
+            );
+          })}
+        </div>
+        <div className={styles.socialsContainer}>
+          <Socials />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/navbar/links.js
@@ -0,0 +1,26 @@
+//Links Object to later be added to navbar
+
+const links = [
+  {
+    url: '#pricing',
+    name: 'Precios',
+  },
+  {
+    url: '#features',
+    name: 'Features',
+  },
+  {
+    url: '#testimonials',
+    name: 'Testimonios',
+  },
+  {
+    url: '#team',
+    name: '¿Quienes Somos?',
+  },
+  {
+    url: '#contact',
+    name: 'Contacto',
+  },
+];
+
+export default links;
