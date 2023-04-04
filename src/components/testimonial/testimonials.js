13f1235c0b6d501a5129fe35043f247b225fb168
efeguerrero <juanfg.8640@gmail.com>
2023-04-04 17:34:29 -0300

    Testimonial Section Done

---
+++ src/components/testimonial/testimonials.js
@@ -0,0 +1,28 @@
+const testimonials = [
+  {
+    name: 'John Doe',
+    title: 'CEO of ABC',
+    img: 'https://source.unsplash.com/50x50/?portrait?1',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    name: 'John Doe',
+    title: 'CEO of ABC',
+    img: 'https://source.unsplash.com/50x50/?portrait?3',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    name: 'John Doe',
+    title: 'CEO of ABC',
+    img: 'https://source.unsplash.com/50x50/?portrait?2',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    name: 'John Doe',
+    title: 'CEO of ABC',
+    img: 'https://source.unsplash.com/50x50/?portrait?4',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+];
+
+export default testimonials;
